288de8fe26dd4a03274fe6098bfc710346fe8e81
pedrodruviaro <pedrodruviaro@gmail.com>
2021-10-29 15:23:23 -0300

    configurando miragejs e axios

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
+import { api } from "../../services/api";
 
 export function TransactionTable() {
+    useEffect(() => {
+        api.get("/transactions").then(({ data }) => console.log(data));
+    }, []);
+
     return (
         <Container>
             <table>
