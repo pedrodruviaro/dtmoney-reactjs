3bc24407587f00d602871e8883bb23e94806f357
pedrodruviaro <pedrodruviaro@gmail.com>
2021-10-30 18:26:47 -0300

    corrigindo estilos e finalizando app

--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -4,13 +4,15 @@ export const Container = styled.div`
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     gap: 2rem;
-    margin-top: -10rem;
+    margin-top: -7rem;
 
     div {
         background-color: var(--shape);
         padding: 1.5rem 2rem;
         border-radius: 0.25rem;
         color: var(--text-light);
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px,
+            rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
 
         header {
             display: flex;
