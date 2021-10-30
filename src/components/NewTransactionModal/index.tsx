92065cf46c4423c8271dac9a4fcd752afba948ec
pedrodruviaro <pedrodruviaro@gmail.com>
2021-10-30 16:29:58 -0300

    finalizando parte estrutural

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,11 @@
 import Modal from "react-modal";
-import { Container } from "./styles";
-import { AiOutlineClose } from "react-icons/ai";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
+import {
+    AiOutlineClose,
+    AiOutlinePlusCircle,
+    AiOutlineMinusCircle,
+} from "react-icons/ai";
+import { useState } from "react";
 
 Modal.setAppElement("#root");
 
@@ -13,6 +18,9 @@ export function NewTransactionModal({
     isOpen,
     onRequestClose,
 }: NewTransactionModalProps) {
+    // states
+    const [type, setType] = useState("deposit");
+
     return (
         <Modal
             isOpen={isOpen}
@@ -32,6 +40,31 @@ export function NewTransactionModal({
                 <h2>Cadastrar Transação</h2>
                 <input type="text" placeholder="Título" />
                 <input type="number" placeholder="Valor" />
+
+                <TransactionTypeContainer>
+                    <RadioBox
+                        type="button"
+                        onClick={() => setType("deposit")}
+                        isActive={type === "deposit"}
+                        activeColor="green"
+                    >
+                        <AiOutlinePlusCircle
+                            style={{ color: "var(--green)" }}
+                        />
+                        <span>Entrada</span>
+                    </RadioBox>
+
+                    <RadioBox
+                        type="button"
+                        onClick={() => setType("withdraw")}
+                        isActive={type === "withdraw"}
+                        activeColor="red"
+                    >
+                        <AiOutlineMinusCircle style={{ color: "var(--red)" }} />
+                        <span>Saída</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
+
                 <input type="text" placeholder="Categoria" />
                 <button type="submit">Cadastrar</button>
             </Container>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
     h2 {
@@ -44,5 +45,52 @@ export const Container = styled.form`
         &:focus {
             filter: brightness(0.9);
         }
+    }
+`;
+
+export const TransactionTypeContainer = styled.div`
+    margin: 1rem 0;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.5rem;
+`;
+
+// Radio Box
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: "green" | "red";
+}
+
+const colors = {
+    red: "#e52e4d",
+    green: "#2ecc71",
+};
+
+export const RadioBox = styled.button<RadioBoxProps>`
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+    border-radius: 0.25rem;
+    background-color: ${(props) =>
+        props.isActive ? transparentize(0.9, colors[props.activeColor]) : ""};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    transition: border-color 200ms ease-in-out;
+
+    &:hover,
+    &:focus {
+        border-color: ${darken(0.1, "#d7d7d7")};
+    }
+
+    span {
+        display: inline-block;
+        margin-left: 1rem;
+        font-size: 1rem;
+        color: var(--text-title);
+    }
+
+    svg {
+        font-size: 1.5rem;
     }
 `;
